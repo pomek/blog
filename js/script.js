c9061a3a973499bba32843a1c578d0eeac02d3d7
Kamil Piechaczek <pomek@pomek.pl>
2015-12-30 23:12:32 +0100

    Added pagination and events for sharing single news in socials

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
-'use strict';
+(function (global) {
+    'use strict';
 
-for (var i = 0; i < 10; ++i) {
-    console.log("#" + i.toString() + ".");
-}
+    var news = document.querySelectorAll('.js-news');
+
+    Array.prototype.forEach.call(news, function (item) {
+        item.querySelector('.js-facebook')
+            .addEventListener('click', function (e) {
+                global.open(this.href, "sharer", "toolbar=0,status=0,width=626,height=436");
+                e.preventDefault();
+            });
+
+        item.querySelector('.js-twitter')
+            .addEventListener('click', function (e) {
+                global.open(this.href, "sharer", "toolbar=0,status=0,width=626,height=436");
+                e.preventDefault();
+            });
+
+        item.querySelector('.js-google')
+            .addEventListener('click', function (e) {
+                global.open(this.href, "sharer", "menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600");
+                e.preventDefault();
+            });
+    });
+})(this);
